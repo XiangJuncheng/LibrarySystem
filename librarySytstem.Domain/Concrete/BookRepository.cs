077f79deec418f506e6bed98f9e7ff402389fb48
ygjch <wwworzzzzzzzzzz@gmail.com>
2017-09-15 16:07:50 +0800

    bug fixx

--- librarySytstem.Domain/Concrete/BookRepository.cs
+++ librarySytstem.Domain/Concrete/BookRepository.cs
@@ -11,20 +11,11 @@ namespace librarySytstem.Domain.Concrete
     public class BookRepository : IBookRepository
     {
 
+        private EFDbContext context = new EFDbContext();
+
         public IQueryable<Book> Books
         {
-            get { return GetBooks().AsQueryable(); }
-        }
-
-        private static List<Book> GetBooks()
-        {
-            //为了演示，这里手工造一些数据，后面会介绍使用EF从数据库中读取。
-            List<Book> books = new List<Book>{
-            new Book { ID = 1, Title = "ASP.NET MVC 4 编程", Price = 52},
-            new Book { ID = 2, Title = "CLR Via C#", Price = 46},
-            new Book { ID = 3, Title = "平凡的世界", Price = 37}
-        };
-            return books;
+            get { return context.Books; }
         }
     }
 }
